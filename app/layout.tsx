fa3f1fe4de8213d4995b70774bfc5b3cd3e453a1
OlegProg2020 <justprog2020@yandex.ru>
2024-04-14 19:26:02 +0300

    Шрифты и цвета

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export async function generateMetadata(): Promise<Metadata> {
 	return {
-		title: 'computed metadata',
+		title: 'Top app',
 		description: '',
 	}
 }
@@ -18,6 +18,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
+			<head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap'
+					rel='stylesheet'
+				/>
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
