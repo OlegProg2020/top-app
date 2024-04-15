cc650ce70919785a4798b14d720044f1832d4554
OlegProg2020 <justprog2020@yandex.ru>
2024-04-15 19:00:50 +0300

    Добавил svgr

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/Button/Button'
 import { Htag } from '@/components/Htag/Htag'
+import Logo from '../public/vercel.svg'
 
 export default function Home() {
 	return (
 		<div>
+			<Logo></Logo>
 			<Htag tag='h1'>Текст</Htag>
 			<Button appearance='primary'>Кнопка</Button>
 			<Button appearance='ghost'>Кнопка</Button>
