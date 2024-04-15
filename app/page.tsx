714fb05de1f3b2a1187311d2e46e5ded7f187263
OlegProg2020 <justprog2020@yandex.ru>
2024-04-15 20:36:48 +0300

    Использовал useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
+'use client'
+
 import { Button } from '@/components/Button/Button'
 import { Htag } from '@/components/Htag/Htag'
 import { P } from '@/components/P/P'
 import { Tag } from '@/components/Tag/Tag'
+import { useState } from 'react'
 import Logo from '../public/vercel.svg'
 
 export default function Home() {
+	const [counter, setCounter] = useState<number>(0)
+
 	return (
 		<div>
 			<Logo></Logo>
-			<Htag tag='h1'>Заголовок</Htag>
+			<Htag tag='h1'>Счетчик: {counter}</Htag>
 			<P>Текст абзаца</P>
-			<Button appearance='primary'>Кнопка</Button>
+			<Button appearance='primary' onClick={() => setCounter((x) => x + 1)}>
+				Увеличить счетчик
+			</Button>
 			<Button appearance='ghost'>Кнопка</Button>
 			<Tag>Hello</Tag>
 			<Tag size='m'>Hello</Tag>
